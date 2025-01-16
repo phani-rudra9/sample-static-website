8943b31ade488eff275bdb61f49a3ac8facaa3e6
phani-rudra9 <53288879+phani-rudra9@users.noreply.github.com>
2025-01-16 20:47:15 +0530

    Update demo.groovy

--- vars/demo.groovy
+++ vars/demo.groovy
@@ -3,8 +3,8 @@ def call(String instanceId, String region, String s3Bucket) {
     withCredentials([aws(credentialsId: 'aws-ssm-credentials', region: region)]) {
         echo "Executing SSM command on instance: ${instanceId} in region: ${region}"
 
-        def ssmCommand = """
-        aws ssm send-command --document-name "AWS-RunShellScript" --targets "Key=instanceids,Values=${instanceId}" --parameters 'commands=["#!/bin/bash",
+        def ssmCommand = """aws ssm send-command --document-name "AWS-RunShellScript" --targets "Key=instanceids,Values=${instanceId}" --parameters 'commands=[
+            "#!/bin/bash",
             "echo \\"Starting installation of AWS CLI...\\"",
             "sudo rm -rf /usr/local/aws* && sudo rm -rf aws*",
             "if ! command -v aws &> /dev/null; then",
@@ -24,7 +24,7 @@ def call(String instanceId, String region, String s3Bucket) {
             "fi",
             "mkdir -p /home/ubuntu/deploy",
             "echo \\"Creating directory for deployment...\\"",
-            "aws s3 cp s3://${s3Bucket}/deploy.zip /home/ubuntu/deploy.zip",
+            "aws s3 cp s3://\${s3Bucket}/deploy.zip /home/ubuntu/deploy.zip",
             "echo \\"Downloaded zip file from S3...\\"",
             "yes | unzip -o /home/ubuntu/deploy.zip -d /home/ubuntu/deploy",
             "export DEBIAN_FRONTEND=noninteractive",
@@ -39,7 +39,7 @@ def call(String instanceId, String region, String s3Bucket) {
             "if [ -f dockerfile ]; then",
             "    export DOCKER_BUILDKIT=1",
             "    sudo docker build -t demo .",
-            "    NONE_IMAGE_ID=$(sudo docker images --filter \\"dangling=true\\" --format \\"{{.ID}}\\" | head -n 1)",
+            "    NONE_IMAGE_ID=\$(sudo docker images --filter \\"dangling=true\\" --format \\"{{.ID}}\\" | head -n 1)",
             "    if [ -n \\"$NONE_IMAGE_ID\\" ]; then",
             "        sudo docker tag \\"$NONE_IMAGE_ID\\" demo:latest",
             "    else",
@@ -50,8 +50,7 @@ def call(String instanceId, String region, String s3Bucket) {
             "    exit 1",
             "fi"
         ]' \
-        --region ${region}
-        """
+        --region ${region}"""
 
         sh ssmCommand
     }
